ecad56bc7390f7eaa1ed4ef2c3a1f74b9458b365
Scott Larribeau <slarribeau@gmail.com>
2015-02-13 15:11:15 -0800

    Clean up feed view.

--- twendy/TrendViewController.m
+++ twendy/TrendViewController.m
@@ -21,19 +21,19 @@
   
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+  return YES;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+  webView.scalesPageToFit = YES;
+  webView.contentMode = UIViewContentModeScaleAspectFit;
 }
-*/
 
+
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
+}
 @end
