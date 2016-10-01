49648c21ecebfb887817bf0f841bd5ebfc7b4e83
slarribeau <slarribeau@gmail.com>
2016-10-01 06:39:38 -0700

    Reset developer and submission certificates; use my oAuth consumer keys instead of codegerms.

--- twendy/AuthenticationModel.m
+++ twendy/AuthenticationModel.m
@@ -10,9 +10,16 @@
 
 @implementation AuthenticationModel
 
-static const NSString *client_id = @"2sVEcZDhudTeScaMShpd3w";
-static const NSString *secret = @"CVqonV4B8wDxSnwzzXCC2uhak8H22R1gXhbsCSF1400"; //codegerms
-static const NSString *callback = @"http://nowandzen.com/callback";
+//static const NSString *client_id = @"2sVEcZDhudTeScaMShpd3w"; //codegerms
+//static const NSString *secret = @"CVqonV4B8wDxSnwzzXCC2uhak8H22R1gXhbsCSF1400"; //codegerms
+//static const NSString *callback = @"http://nowandzen.com/callback";
+
+
+static const NSString *client_id = @"3gVLXgS5yRw9L1LwuO49mT2UN"; //slarribeau
+static const NSString *secret = @"cEsD1MLgWwgnkddxjyN41DkYiaaGS8Dt6jITfeb8UrW4L85MfU"; //slarribeau
+
+static const NSString *callback = @"http://nowandzen2.com/callback";
+
 
 static BOOL isLoggedInBOOL = NO;
 static BOOL initialized = NO;
